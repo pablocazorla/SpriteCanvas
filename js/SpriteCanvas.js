ba322544af5f32e76e6f8e6d3ed7f48107cd8a61
pablocazorla <pablo.david.cazorla@gmail.com>
2012-11-16 16:58:34 -0300

    with Render

--- js/SpriteCanvas.js
+++ js/SpriteCanvas.js
@@ -1,103 +1,123 @@
 //SpriteCanvas
 ;(function(){
 		
-	//Utils
-	if(!window.Object.extend){
-		window.Object.prototype.extend = function(source) {
-			for (var property in source) {
-				if (source[property] && source[property].constructor && source[property].constructor === Object) {
-					this[property] = this[property] || {};
-					arguments.callee(this[property], source[property]);
-				}else{
-					this[property] = source[property];
-				};
-			};
-			return this;
-		};
-	}
 	
-	var SpriteC = function(){
-	
+	var SpriteConstructor = function(custom){
+		this.init(custom);
 	};
 	
-	SpriteC.prototype = {
-		conf : {			
-			x : 0,
-			y : 0,
-			draw : function(c){}
-		},
-		c : null,
-		parent : null,
+	SpriteConstructor.prototype = {
+		conf : null,
 		collection : [],
 		length : 0,
+		
 		init : function(custom){
-			this.conf.extend(custom);
+			this.conf = {			
+				x : 0,
+				y : 0,
+				xScale : 1,
+				yScale : 1,
+				rotation : 0,
+				draw : function(c){}
+			};
+			this.conf = extendObject(this.conf,custom);
+			
+			this.collection = [];
+			this.length = 0;
 			return this;
 		},
 		add : function(sprite){
-			sprite.parent = this;
-			sprite.c = this.c;
 			this.collection.push(sprite);
 			this.length++;
 			return this;
 		},
-		render : function(){
-			this.conf.draw(this.c);
+		animate : function(props,duration){
+			
+		},
+		render : function(stage){
+			
+			//Set transform
+			stage.c.translate(this.conf.x,this.conf.y);
+			stage.c.scale(this.conf.xScale,this.conf.yScale);
+			stage.c.rotate(this.conf.rotation*Math.PI/180);
+			
+			this.conf.draw(stage.c);
 			
 			for(var i=0;i<this.length;i++){
-				this.collection[i].render();
+				this.collection[i].render(stage);
 			}
 			return this;
 		}
 	};
 	
-	var StageC = function(id){
+	var StageConstructor = function(id){
 		this.init(id);
 	};
 	
-	StageC.prototype = {
+	StageConstructor.prototype = {
 		id : null,
 		canvas : null,
 		c : null,
-		mainSprite : null,
+		collection : [],
+		length : 0,
+		cTransform : {},
 		
 		init : function(id){
 			this.id = id;
 			this.canvas = document.getElementById(id);
 			this.c = this.canvas.getContext('2d');
-			
-			this.mainSprite = new Sprite();
-			this.mainSprite.c = this.c;
+			this.collection = [];
+			this.length = 0;
+			this.cTransform = {
+				x : 0,
+				y : 0,
+				xScale : 1,
+				yScale : 1,
+				rotation : 0
+			};
 			this.render();
 			return this;
 		},
 		render : function(){
 			var self = this;
 			setInterval(function(){
-			//	self.c.clearRect(0,0,self.canvas.width,self.canvas.height);
-				self.mainSprite.render();
+				self.c.clearRect(0,0,self.canvas.width,self.canvas.height);
+				for(var i=0;i<self.length;i++){
+					
+					//Reset C
+					self.c.setTransform(1,0,0,1,0,0);
+					self.cTransform = {
+						x : 0,
+						y : 0,
+						xScale : 1,
+						yScale : 1,
+						rotation : 0
+					};
+					
+					//Render branch
+					self.collection[i].render(self);
+				}
 			},20);
 			return this;
 		},
 		add : function(sprite){
-			this.mainSprite.add(sprite);
+			this.collection.push(sprite);
+			this.length++;
 			return this;
 		},
 		quit : function(sprite){
-			this.mainSprite.quit(sprite);
+			//
 			return this;
 		}
 	};	
 	
 	//Shortcut
 	if(!window.Sprite){
-		window.Sprite = function(custom){
-			var newS = new SpriteC(custom);
-			newS.init(custom);
-			return newS;
+		window.Sprite = function(custom){			
+			return new SpriteConstructor(custom);
 		};
 		window.Stage = function(custom){
-			return new StageC(custom);
+			return new StageConstructor(custom);
 		};
 	};	
 	
